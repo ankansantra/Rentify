d4026f46f39a1aac8dccd128d65e1f2bd3425d82
Ankan Santra <123286549+ankansantra@users.noreply.github.com>
2024-05-21 00:22:49 +0530

    first commit

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+      Find your perfect home away from home <br /> and create unforgettable experiences.
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Slide from '../components/Slide'
+import Categories from '../components/Categories'
+import Footer from '../components/Footer'
+import Listings from '../components/Listings'
+
+const HomePage = () => {
+  return (
+    <>
+        <Navbar/>
+        <Slide/>
+        <Categories/>
+        <Listings/>
+        <Footer/>
+    </>
+  )
+}
+
+export default HomePage
